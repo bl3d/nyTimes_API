94295a4b30eb935395c5c829b007c7835d322ac2
London-Reed <london.reed1911@gmail.com>
2017-03-04 13:32:59 -0500

    started api call

--- assets/javascript/backend.js
+++ assets/javascript/backend.js
@@ -1,6 +1,30 @@
 (function() {
   'use strict'; 
 
-  console.log('backend... ready to go!');
+
+//Input from user
+
+var searchTerm = 
+
+
+var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+url += '?' + $.param({
+  'api-key': "d20e3e965a6e439187a9e32695566dfc",
+  'q': "soccer",
+  'begin_date': "20170304",
+  'end_date': "20170304"
+});
+$.ajax({
+  url: url,
+  method: 'GET',
+}).done(function(result) {
+  console.log(result);
+}).fail(function(err) {
+  throw err;
+
+   console.log(result);
+});
+
+ 
 
 }()); 
