213b5803f9d65b6d0ed696ebaad87ec3b91ddeaf
bl3d <mr.brentlewis@gmail.com>
2017-03-04 13:52:06 -0500

    testing search function request

--- assets/javascript/frontend.js
+++ assets/javascript/frontend.js
@@ -1,6 +1,16 @@
 (function() {
   'use strict'; 
 
-  console.log('frontend... ready to go!');
+  $('#search-btn').on('click', function(e){
+  	// searchTerm, beginDate, endDate, numResults
+  	e.preventDefault();
+  	var searchTerm = $('#search').val();
+  	var beginDate = parseInt($('#start-year').val());
+  	var endDate = parseInt($('#end-year').val());
+  	var numResults = parseInt($('#num-records').find('option:selected').attr('value')); 
+  	
+  	console.log(searchTerm, beginDate, endDate, numResults);
+  	search(searchTerm, beginDate, endDate, numResults);
+  });
 
 }()); 
